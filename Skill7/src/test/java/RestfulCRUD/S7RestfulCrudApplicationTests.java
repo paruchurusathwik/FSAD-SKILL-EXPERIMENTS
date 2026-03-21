8b5673cb7468638d31fd590a3225ab338823d520
paruchurusathwik <2400031061@kluniversity.in>
2026-03-21 19:38:29 +0530

    Added Skill7

---
+++ Skill7/src/test/java/RestfulCRUD/S7RestfulCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package RestfulCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7RestfulCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
